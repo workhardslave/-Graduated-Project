aa630ba52f9583c7031936b65f504430b6e71449
workhardslave <mae0428@naver.com>
2020-05-21 22:34:32 +0900

    style: Leave comments

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,18 +1,18 @@
-package com.example.demo;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@EnableJpaAuditing
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DemoApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.example.demo;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@EnableJpaAuditing
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class DemoApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
