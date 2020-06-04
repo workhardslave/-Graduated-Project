963b097dd353f8db523d4fb49d916bbc95ff3d05
da-nyee <leede0418@likelion.org>
2020-06-04 19:05:05 +0900

    feat: Keep developing Disease info visualization (admin)

--- src/main/java/com/example/demo/disease/controller/DiseaseController.java
+++ src/main/java/com/example/demo/disease/controller/DiseaseController.java
@@ -1,18 +1,32 @@
 package com.example.demo.disease.controller;
 
+import com.example.demo.disease.dto.DiseaseResponseDto;
+import com.example.demo.disease.service.DiseaseService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
 
 @Controller
 @AllArgsConstructor
 @Slf4j
 public class DiseaseController {
+
+    DiseaseService diseaseService;
     
     // 반려견 질병 정보 조회 페이지
     @GetMapping("/admin/disease/info")
-    public String DiseaseInfoPage() {
+    public String DiseaseInfoPage(Model model) {
+//        List<DiseaseResponseDto> diseases = diseaseService.findAllDesc();
+        Map<String, Integer> diseases = new TreeMap<>();
+        diseases.put("감기", 11);
+        diseases.put("바이러스", 8);
+        diseases.put("피부병", 5);
+        model.addAttribute("diseases", diseases);
+
         return "disease/diseaseInfo";
     }
 }
---
+++ src/main/java/com/example/demo/disease/service/DiseaseService.java
@@ -0,0 +1,24 @@
+package com.example.demo.disease.service;
+
+import com.example.demo.disease.dao.DiseaseRepository;
+import com.example.demo.disease.dto.DiseaseResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class DiseaseService {
+
+    private final DiseaseRepository diseaseRepository;
+
+    @Transactional
+    public List<DiseaseResponseDto> findAllDesc() {
+        return diseaseRepository.findAllDesc().stream()
+                .map(DiseaseResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
