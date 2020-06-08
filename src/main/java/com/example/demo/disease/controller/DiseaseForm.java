b5775dd80e0abce2ecd002e374d88817abadbed1
jeonhyeonu <tkawnd111@gmail.com>
2020-06-08 22:06:09 +0900

    feat : The Outside Server(FLASK) linked SpringBoot Project!!

---
+++ src/main/java/com/example/demo/disease/controller/DiseaseForm.java
@@ -0,0 +1,19 @@
+package com.example.demo.disease.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class DiseaseForm {
+
+    @NotEmpty(message = "증상 5개를 선택해주세요")
+    private String name1;
+    private String name2;
+    private String name3;
+    private String name4;
+    private String name5;
+
+}
--- src/main/java/com/example/demo/member/vo/controller.java
+++ src/main/java/com/example/demo/member/vo/controller.java
@@ -1,19 +1,80 @@
 package com.example.demo.member.vo;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
-@Controller
-@AllArgsConstructor
+import java.util.HashMap;
+
+
+@RestController
 public class controller {
 
-    @Autowired
-    RestTemplateTestService restTemplateTestService;
 
-    @PostMapping(value="/please")
-    public void test(){
-        restTemplateTestService.callPostExternalServer();
+    @GetMapping("/abc")
+    public String callAPI() throws JsonProcessingException {
+
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        String jsonInString = "";
+
+        try {
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            HttpHeaders header = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(header); //값받기
+
+            String url = "http://localhost:80/test";
+
+//            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"key=430156241533f1d058c603178cc3ca0e&targetDt=20120101").build();
+
+            ResponseEntity<Object> resultMap = restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
+            result.put("Statuscode", resultMap.getStatusCodeValue());
+            result.put("header", resultMap.getHeaders());
+            result.put("body", resultMap.getBody());
+            ObjectMapper mapper = new ObjectMapper();
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body", e.getStatusText());
+            System.out.println("dfdfdfdf");
+            System.out.println(e.toString());
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body", "excpetion오류");
+            System.out.println(e.toString());
+        }
+
+        System.out.println("확인해보기");
+        System.out.println(jsonInString);
+        return jsonInString;
+
+    }
+
+    @PostMapping("/put")
+    public void callAPI_put() throws JsonProcessingException {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:80/test";
+        String name = "abc";
+        String password="1234";
+
+        MultiValueMap<String,String> parameters = new LinkedMultiValueMap<String, String>();
+        parameters.add("j_username", name);
+        parameters.add("j_password", password);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url,parameters,String.class);
+        System.out.println("--------------------------");
+        System.out.println(responseEntity);
+
     }
 }
