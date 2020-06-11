86192a3f13ff7dc701258a69bef275e2280ddfc9
daeun <leede0418@likelion.org>
2020-06-11 18:59:53 +0900

    fix: Keep developing Disease info visualization (admin)

--- src/main/java/com/example/demo/disease/dto/DiseaseResponseDto.java
+++ src/main/java/com/example/demo/disease/dto/DiseaseResponseDto.java
@@ -8,10 +8,12 @@ public class DiseaseResponseDto {
     private String name;
     private String symptom;
     private String type;
+    private String description;
 
     public DiseaseResponseDto(Disease entity) {
         this.name = entity.getName();
         this.symptom = entity.getSymptom();
         this.type = entity.getType();
+        this.description = entity.getDescription();
     }
 }
