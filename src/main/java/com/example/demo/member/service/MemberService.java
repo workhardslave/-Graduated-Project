fe0175f5264e7481f2e19d82e958b47d6a0de2f8
workhardslave <mae0428@naver.com>
2020-06-16 21:13:11 +0900

    feat : Add radio buttons to choose a role

--- src/main/java/com/example/demo/member/service/MemberService.java
+++ src/main/java/com/example/demo/member/service/MemberService.java
@@ -54,8 +54,13 @@ public class MemberService implements UserDetailsService {
     public Long SignUp(MemberSaveRequestDto memberDto) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         memberDto.SHA256_PassWord(passwordEncoder.encode(memberDto.getPassword()));
-//        memberDto.GIVE_Role(Role.GUEST);
-        memberDto.GIVE_Role(Role.ADMIN);
+//        memberDto.GIVE_Role(Role.ADMIN);
+
+        if(memberDto.getRole() == Role.GUEST) {
+            memberDto.GIVE_Role(Role.GUEST);
+        } else if(memberDto.getRole() == Role.VET) {
+            memberDto.GIVE_Role(Role.VET);
+        }
 
         return memberRepository.save(memberDto.toEntity()).getId();
     }
