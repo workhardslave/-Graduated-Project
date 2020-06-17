b2e00492f28407b9148322ecf806fb9d0cb0dfdb
workhardslave <mae0428@naver.com>
2020-06-17 16:23:39 +0900

    feat : Add a hospital registeration page and fix a issue of map click

--- src/main/java/com/example/demo/hospital/controller/HospitalController.java
+++ src/main/java/com/example/demo/hospital/controller/HospitalController.java
@@ -1,4 +1,23 @@
 package com.example.demo.hospital.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@Controller
+@Slf4j
 public class HospitalController {
+
+    // 병원 등록 페이지
+    @GetMapping("/hospital/registeration/")
+    public String registeration() {
+
+        return "hospital/registeration";
+    }
 }
